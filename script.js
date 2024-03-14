1440ecb1cc3c12891777a24eac2cf62b6bcdc66c
elena_e <elenaka123@gmail.com>
2024-03-14 08:19:28 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const cat = {};
+
+cat.name = "Fred";
+cat.nickname = "Flooficus";
+cat.age = 5;
+cat.isSleeping = true;
+cat.favoriteToys = ["spring", "ping pong balls", "bird stuffy"]
+cat.pet = function () {
+    return "purrrrrr";
+}
+
+console.log(cat);
